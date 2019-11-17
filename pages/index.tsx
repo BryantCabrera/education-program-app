c5b9cc35521c9b0e58422d25802de2a48e0543b9
Bryant Cabrera <bcabrera@gocurrency.com>
2019-11-17 08:24:58 -0800

    Creates Layout component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 export default () => (
 	<div className="app">
 		<header>
-			<h1>Education Programs</h1><h2>By: Bryant Cabrera</h2>
+			<h1>Education Programs</h1>
+			<h2>By: Bryant Cabrera</h2>
 		</header>
 	</div>
 )
