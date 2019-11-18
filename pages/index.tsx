408289b7c6c79b277430b9756903af9f5908ce67
Bryant Cabrera <bcabrera@gocurrency.com>
2019-11-17 22:44:22 -0800

    Converts index.tsx into a class-based react component and imports tool for fetch calls.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import React, { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
-export default () => (
-	<Layout>
-		<p>Sample Text</p>
-	</Layout>
-);
+export default class extends Component {
+	static async getInitialProps() {
+		const res = await fetch('http://localhost:4000/programs');
+		const programs = await res.json();
+		return { programs };
+	}
+	render() {
+		return (
+			<Layout>
+				<p>Sample Text</p>
+			</Layout>
+		)
+	}
+};
