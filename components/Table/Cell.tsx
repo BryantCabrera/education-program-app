06931b52c81590b562a5f7ac5d41851a3974c2b0
Bryant Cabrera <bcabrera@gocurrency.com>
2019-11-22 15:58:01 -0800

    Adds custom td element for program and school.

--- components/Table/Cell.tsx
+++ components/Table/Cell.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { ITableCellProps } from '../../common/interfaces';
+import styled from 'styled-components';
 
 export const Cell = (props: ITableCellProps) => {
 	const CustomTd = props.customTd;
+
+	const ProgramName = styled.span`
+		font-size: 1.5em;
+	`;
+	const SchoolName = styled.span`
+		font-size: 1em;
+	`;
 
 	return (
 		<tr>
@@ -15,6 +23,16 @@ export const Cell = (props: ITableCellProps) => {
 					}).reduce((result, item) => {
 						return item;
 					}, {}).custd;
+
+				if (item === 'programName') {
+					return (
+						<td key={id}>
+							<ProgramName>{props.tdData[item]}</ProgramName>
+							<br />
+							<SchoolName>{props.tdData['school']}</SchoolName>
+						</td>
+					);
+				}
 
 				if (!CustomTd) {
 					return <td key={id}>{props.tdData[item]}</td>;
